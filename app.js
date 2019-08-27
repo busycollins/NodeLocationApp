af465acc1fd9918943c4407183f15d71f3e017c5
chibuike.nwaogugu <chibuike.nwaogugu@venturegardengroup.com>
2019-08-27 09:59:35 +0100

    intial commit

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
+require('./app_api/models/db');
+
+//var indexRouter = require('./app_server/routes/index');
+//var usersRouter = require('./app_server/routes/users');
+const apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -19,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
